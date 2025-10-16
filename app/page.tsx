0b018f7976f2a4a594c9da65dc23eec90c28e097
Laura .__. <lauraquesada880@gmail.com>
2025-10-15 23:16:33 -0500

    Cambiar enlace de Log in a /dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -28,13 +28,12 @@ export default function Page() {
           <div
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
-<Link
-  href="/dashboard"
-  className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
->
-  <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-</Link>
-
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
